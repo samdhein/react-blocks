06fb2ccc3e17251000e50b24f81b13f41c0f319a
samdhein <hein.sam@gmail.com>
2022-01-13 00:00:44 -0800

    update 1

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,9 +1,22 @@
 import React, {Component} from 'react';
 import styles from './styling.module.css';
+import Subcontent from './subcontent'
+import Advertisement from './advertisement'
 
 class Main extends Component {
     render() {
-        return <div className={ styles.main} > </div>
+        return (
+        <div className={ styles.main} > 
+            {/* <div className= {styles.flex}>
+                <Subcontent />
+                <Subcontent />
+                <Subcontent />
+            </div>
+            <div>
+                <Advertisement />
+            </div> */}
+        </div>
+        )
     }
 }
 
